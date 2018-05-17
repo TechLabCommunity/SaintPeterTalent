8d84555a4a6057ad566b8b046d706c321a1ca7a6
Andrea Tazio Giusti <andreatgiusti@gmail.com>
2018-05-17 22:18:54 +0200

    better sketch arduino

--- wiegand_sketch/wiegand_sketch.ino
+++ wiegand_sketch/wiegand_sketch.ino
@@ -4,15 +4,11 @@
 #include <avr/wdt.h>
 #define N_PINS 3
 #define DOOR_PIN 12
-#define TIMES_TO_RESET 40
-#define Reset_AVR() wdt_enable(WDTO_30MS); while(1) {} 
 
 
 const unsigned PINS[N_PINS] = {4,5,6};
 WIEGAND wg;
-unsigned int code = 0;
 volatile unsigned int count_res = 0;
-unsigned int input_times = 0;
 
 void turn_off(){
   for (int i=0; i<N_PINS; i++){
@@ -39,17 +35,11 @@ void setup() {
 void loop() {
   if (wg.available())
   {
-    code = wg.getCode();
+    unsigned long code = wg.getCode();
     if (code > 999) {
       Serial.println(String(code) + "#");
     }
-    code = 0;
     Serial.flush();
-    input_times++;
-    if (input_times >= TIMES_TO_RESET){
-      input_times = 0;
-      Reset_AVR();
-    }
   }else if (Serial.available()){
     char c = (char)Serial.read();
     switch(c){
