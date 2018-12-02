bff9cedf65b8bce78815680a6c07d130af598b75
Andrea Giusti <andrea.giusti@enteeractive.com>
2018-12-02 17:57:04 +0100

    Added docker and develop mode

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ from module.SpmMain import SpmMain
 from module.SpmSerial import SpmSerial
 from module.SpmAlarm import SpmAlarm
 from module.SpmUpdateAccessCode import SpmUpdateAccessCode
+import sys
 
-SpmMain().start()
-SpmSerial().start()
-SpmAlarm().start()
-SpmUpdateAccessCode().start()
+def main():
+    SpmMain().start()
+    SpmUpdateAccessCode().start()
+    if not (len(sys.argv) > 1 and sys.argv[1] == '-develop'):
+        SpmSerial().start()
+        SpmAlarm().start()
+
+if __name__ == "__main__":
+    main()
