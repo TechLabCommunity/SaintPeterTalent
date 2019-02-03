eb80237de4d73f3a99e82e02edb714f5057bd559
Andrea Giusti <andrea.giusti@enteeractive.com>
2019-02-03 18:46:52 +0100

    release sketch

--- wiegand_sketch/saint_peter_wiegand/src/main.cpp
+++ wiegand_sketch/saint_peter_wiegand/src/main.cpp
@@ -2,8 +2,6 @@
 #include <Wiegand.h>
 #include <Wire.h>
 #include <MsTimer2.h>
-#include <avr/io.h>
-#include <avr/wdt.h>
 #include <LiquidCrystal_I2C.h>
 #define DOOR_PIN 12
 #define COLS 20
@@ -11,18 +9,11 @@
 #define ADDR_I2C 0x27
 #define SEPARATOR '|'
 #define TIMEOUT_WAIT 5000
-#define TIME_RESET_LCD 15 * 60 * 1000
-#define Reset_AVR()      \
-  wdt_enable(WDTO_30MS); \
-  while (1)              \
-  {                      \
-  }
-
+#define TIME_RESET_LCD 900000UL //15 Minutes
 #ifndef MAGIC_NUMBER
 #define MAGIC_NUMBER 0 //define into platformio.ini
 #endif
 #define STATUS_MEMORY_CODE 999998
-
 #define RESPONSE_BYPASS "s||||||BYPASS|0"
 
 typedef struct
@@ -33,31 +24,17 @@ typedef struct
 } InfoMember;
 
 WIEGAND wg;
-unsigned int count_res = 0;
 unsigned long code = 0;
 String response = "";
-LiquidCrystal_I2C lcd(0x27, 20, 4);
+LiquidCrystal_I2C lcd(0x27, COLS, ROWS);
 volatile bool need_reset_lcd = false;
+int freeRam();
 
-int freeRam()
-{
-  extern int __heap_start, *__brkval;
-  int v;
-  return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval);
-}
+InfoMember get_infos(const String response, const char delimiter = SEPARATOR);
 
-InfoMember get_infos(String response, char delimiter = SEPARATOR);
+void print_rows(const String s1 = "", const String s2 = "", const String s3 = "", const String s4 = "");
 
-void print_rows(String s1, String s2, String s3, String s4);
-
-void reset_lcd()
-{
-  need_reset_lcd = true;
-  Wire.end();
-  lcd.begin();
-  print_rows(String("    TalentLab CA"), String(""), String("    In funzione"), String(""));
-  need_reset_lcd = false;
-}
+void reset_lcd();
 
 void setup()
 {
@@ -67,7 +44,7 @@ void setup()
   wg.begin();
   lcd.begin();
   lcd.backlight();
-  print_rows(String("    TalentLab CA"), String(""), String("    In funzione"), String(""));
+  print_rows("TalentLab CA", "", "In funzione");
   MsTimer2::set(TIME_RESET_LCD, reset_lcd);
   MsTimer2::start();
 }
@@ -103,17 +80,17 @@ void loop()
     switch (member.action)
     {
     case 'c':
-      print_rows(String("ACCESSO NEGATO"), String("   ERRORE VINCOLO"), String("     NO PARTNER"), String(""));
+      print_rows("ACCESSO NEGATO", "ERRORE VINCOLO", "NO PARTNER");
       break;
     case 'b':
-      print_rows(String("    CODICE ERRATO"), String("    --RIPROVARE--"), member.custom_message, String(""));
+      print_rows("CODICE ERRATO", "--RIPROVARE--", member.custom_message);
       break;
     case 's':
       if (member.alarm_status == "2")
       {
-        alarm_msg = "     ALLARME OFF";
+        alarm_msg = "ALLARME OFF";
       }
-      print_rows(String("   CODICE VALIDO"), String("    --ENTRATA--"), member.custom_message, String(alarm_msg));
+      print_rows("CODICE VALIDO", "--ENTRATA--", member.custom_message, alarm_msg);
       digitalWrite(DOOR_PIN, HIGH);
       break;
     case 'a':
@@ -121,7 +98,7 @@ void loop()
       {
         alarm_msg = "     ALLARME ON";
       }
-      print_rows(String("   CODICE VALIDO"), String("     --USCITA--"), member.custom_message, String(alarm_msg));
+      print_rows("CODICE VALIDO", "--USCITA--", member.custom_message, alarm_msg);
       break;
     default:
       break;
@@ -130,67 +107,92 @@ void loop()
     code = 0;
     delay(TIMEOUT_WAIT);
     digitalWrite(DOOR_PIN, LOW);
-    Wire.end();
-    lcd.begin();
-    print_rows(String("    TalentLab CA"), String(""), String("    In funzione"), String(""));
+    reset_lcd();
   }
 }
 
-InfoMember get_infos(String response, char delimiter)
+InfoMember get_infos(const String response, const char delimiter)
 {
   unsigned int i = 0;
   int col = 0;
   InfoMember member;
   member.action = response[0];
-  int c = 0;
-  while (i < response.length() && response[i] != '\n' && c < 80)
+  while (i < response.length() && response[i] != '\n')
   {
     if (response[i] == delimiter)
     {
       col++;
-      i++;
-      continue;
     }
-    switch (col)
+    else
     {
-    case 1:
-      member.name += response[i];
-      break;
-    case 2:
-      member.surname += response[i];
-      break;
-    case 3:
-      member.type_enter = ((String)response[i]).toInt();
-      break;
-    case 4:
-      member.code += response[i];
-      break;
-    case 5:
-      member.alarm_status += response[i];
-      break;
-    case 6:
-      member.custom_message += response[i];
-      break;
+      switch (col)
+      {
+      case 1:
+        member.name += response[i];
+        break;
+      case 2:
+        member.surname += response[i];
+        break;
+      case 3:
+        member.type_enter = ((String)response[i]).toInt();
+        break;
+      case 4:
+        member.code += response[i];
+        break;
+      case 5:
+        member.alarm_status += response[i];
+        break;
+      case 6:
+        member.custom_message += response[i];
+        break;
+      }
     }
     i++;
-    c++;
   }
   return member;
 }
 
-void print_rows(String s1, String s2, String s3, String s4)
+void print_rows(const String s1, const String s2, const String s3, const String s4)
 {
   lcd.clear();
-  lcd.setCursor(0, 0);
-  s1.substring(0, 19);
-  lcd.print(s1);
-  lcd.setCursor(0, 1);
-  s2.substring(0, 19);
-  lcd.print(s2);
-  lcd.setCursor(0, 2);
-  s3.substring(0, 19);
-  lcd.print(s3);
-  lcd.setCursor(0, 3);
-  s4.substring(0, 19);
-  lcd.print(s4);
+  String rows[ROWS];
+  rows[0] = s1;
+  rows[1] = s2;
+  rows[2] = s3;
+  rows[3] = s4;
+  for (unsigned int i = 0; i < ROWS; i++)
+  {
+    lcd.setCursor(0, i);
+    if (rows[i].length() > COLS)
+    {
+      rows[i].substring(0, 19);
+    }
+    else
+    {
+      unsigned offset = (COLS - rows[i].length()) / 2;
+      for (unsigned int i = 0; i < offset; i++)
+      {
+        lcd.print(" ");
+      }
+    }
+    lcd.print(rows[i]);
+  }
+}
+
+void reset_lcd()
+{
+  if (need_reset_lcd)
+    return;
+  need_reset_lcd = true;
+  Wire.end();
+  lcd.begin();
+  print_rows("TalentLab CA", "", "In funzione");
+  need_reset_lcd = false;
+}
+
+int freeRam()
+{
+  extern int __heap_start, *__brkval;
+  int v;
+  return (int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval);
 }
